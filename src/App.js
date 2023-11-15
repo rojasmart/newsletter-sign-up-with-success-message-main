3e17be32a70618832641a1c13d00b2700198ed89
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-11-15 21:45:02 +0000

    added root style and html structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React from "react";
+import Img from "./assets/images/illustration-sign-up-desktop.svg";
+import Icon from "./assets/images/icon-list.svg";
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="container">
+      <div className="card">
+        <div className="content">
+          <h1 className="content-title">Stay updated!</h1>
+          <p className="content-subtitle">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+          <div className="content-list">
+            <ul>
+              <li>
+                <img src={Icon} alt="content-icon" />
+                Product discovery and building what matters
+              </li>
+              <li>
+                <img src={Icon} alt="content-icon" />
+                Measuring to ensure updates are a success
+              </li>
+              <li>
+                <img src={Icon} alt="content-icon" />
+                And much more!
+              </li>
+            </ul>
+          </div>
+          <div className="content-input">
+            <label htmlFor="content-text-input">Email address</label>
+            <input type="text" placeholder="email@company.com" />
+            <button className="content-button">
+              Subscribe to monthly newsletter
+            </button>
+          </div>
+        </div>
+        <div className="image">
+          <img src={Img} alt="img-sign-up" />
+        </div>
+      </div>
     </div>
   );
 }
