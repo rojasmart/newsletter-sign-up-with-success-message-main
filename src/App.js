e4d1f683490ea1546ef01890c3d2f7027d2077fd
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-11-18 14:31:09 +0000

    card-success layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [email, setEmail] = useState("");
   const [error, setError] = useState(null);
   const [submit, setSubmit] = useState(false);
+  const [validate, setValidate] = useState(false);
 
   function isValidEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
@@ -27,6 +28,7 @@ function App() {
 
     if (isValidEmail(email)) {
       console.log("the email is valid");
+      setValidate(true);
     } else {
       setError("Valid email required");
     }
@@ -35,56 +37,65 @@ function App() {
 
   return (
     <div className="container">
-      <div className="card">
-        <div className="content">
-          <h1 className="content-title">Stay updated!</h1>
-          <p className="content-subtitle">
-            Join 60,000+ product managers receiving monthly updates on:
-          </p>
-          <div className="content-list">
-            <ul>
-              <li>
-                <img src={Icon} alt="content-icon" />
-                Product discovery and building what matters
-              </li>
-              <li>
-                <img src={Icon} alt="content-icon" />
-                Measuring to ensure updates are a success
-              </li>
-              <li>
-                <img src={Icon} alt="content-icon" />
-                And much more!
-              </li>
-            </ul>
-          </div>
-          <div className="content-input">
-            <form onSubmit={handleSubmit}>
-              <div className="content-labels">
-                <label htmlFor="content-text-input">Email address</label>
-                {error && (
-                  <label htmlFor="content-text-error" className="email-error">
-                    {error}
-                  </label>
-                )}
+      <div className={validate ? "card-success" : "card"}>
+        {validate ? (
+          <div className="content-success"></div>
+        ) : (
+          <>
+            <div className="content">
+              <h1 className="content-title">Stay updated!</h1>
+              <p className="content-subtitle">
+                Join 60,000+ product managers receiving monthly updates on:
+              </p>
+              <div className="content-list">
+                <ul>
+                  <li>
+                    <img src={Icon} alt="content-icon" />
+                    Product discovery and building what matters
+                  </li>
+                  <li>
+                    <img src={Icon} alt="content-icon" />
+                    Measuring to ensure updates are a success
+                  </li>
+                  <li>
+                    <img src={Icon} alt="content-icon" />
+                    And much more!
+                  </li>
+                </ul>
               </div>
-              <input
-                type="text"
-                placeholder="email@company.com"
-                id="email"
-                name="email"
-                value={email}
-                onChange={handleChange}
-                className={error ? "error-input" : "error"}
-              />
-              <button className="content-button" type="submit">
-                Subscribe to monthly newsletter
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="image">
-          <img src={Img} alt="img-sign-up" />
-        </div>
+              <div className="content-input">
+                <form onSubmit={handleSubmit}>
+                  <div className="content-labels">
+                    <label htmlFor="content-text-input">Email address</label>
+                    {error && (
+                      <label
+                        htmlFor="content-text-error"
+                        className="email-error"
+                      >
+                        {error}
+                      </label>
+                    )}
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="email@company.com"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                    className={error ? "error-input" : "error"}
+                  />
+                  <button className="content-button" type="submit">
+                    Subscribe to monthly newsletter
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="image">
+              <img src={Img} alt="img-sign-up" />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
